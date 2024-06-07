638f720b657c059b73e31da224e77efdad27aeac
Rajat Sharma <rxx256@gmail.com>
2024-06-08 00:15:06 +0530

    use media-parser worker

--- server/handlers/media.js
+++ server/handlers/media.js
@@ -3,6 +3,7 @@ import { join } from 'path';
 import { createWriteStream, existsSync } from 'fs';
 import { rename, mkdir, readdir } from 'fs/promises';
 import { pipeline } from 'stream';
+import { fork } from 'child_process';
 import config from '../../config.js';
 import logger from '../../utils/logger.js';
 import {
@@ -176,12 +177,6 @@ export async function handleMediaUpload_v1(request, reply) {
     /** @type {string} */
     let originalVideoFilePath;
 
-    /** @type {number} */
-    let videoWidth;
-
-    /** @type {number} */
-    let videoHeight;
-
     // Allow only mp4 file format
     if (data.mimetype !== 'video/mp4') {
         return reply.status(400).send({
@@ -207,104 +202,41 @@ export async function handleMediaUpload_v1(request, reply) {
     // Write video stream
     await pump(data.file, createWriteStream(originalVideoFilePath));
     await updateMediaStatus(mediaId, 'uploaded');
-    await generateVideoThumbnail(originalVideoFilePath, videoBucket);
 
-    // Get original video resolution
-    try {
-        const _resolution = await getVideoResolution(originalVideoFilePath);
+    const proc = fork('workers/media-parser', [videoBucket]);
 
-        videoWidth = _resolution.width;
-        videoHeight = _resolution.height;
-        await setMediaResolution(mediaId, [videoWidth, videoHeight]);
-    } catch (error) {
-        logger.log({
-            level: 'error',
-            label: 'handler:media',
-            message: `Failed to get resolution error="${error.message}"`,
-        });
-        return reply.status(500).send({
-            message: 'Failed to get video resolution',
-        });
-    }
-
-    await updateMediaStatus(mediaId, 'converting');
-
-    // Convert 1080p resolution
-    if (videoHeight > 1080 && videoWidth > 1920) {
-        try {
-            await convertVideoResolution(
-                originalVideoFilePath,
-                join(videoBucket, '1080p.mp4'),
-                '1920x1080',
-            );
-            logger.log({
-                level: 'info',
-                label: 'handler:media',
-                message: `Video ${mediaId} converted to 1080p`,
-            });
-        } catch (error) {
-            logger.log({
-                level: 'error',
-                label: 'handler:media',
-                message: `Failed to convert 1080p video error="${error.message}"`,
-            });
-        }
-    }
-
-    // Convert 720p resolution
-    if (videoHeight > 720 && videoWidth > 1280) {
-        try {
-            await convertVideoResolution(
-                originalVideoFilePath,
-                join(videoBucket, '720p.mp4'),
-                '1280x720',
-            );
-            logger.log({
-                level: 'info',
-                label: 'handler:media',
-                message: `Video ${mediaId} converted to 720p`,
-            });
-        } catch (error) {
-            logger.log({
-                level: 'error',
-                label: 'handler:media',
-                message: `Failed to convert 720p video error="${error.message}"`,
-            });
-        }
-    }
-
-    // Convert 480p resolution
-    if (videoHeight > 480 && videoWidth > 854) {
-        try {
-            await convertVideoResolution(
-                originalVideoFilePath,
-                join(videoBucket, '480p.mp4'),
-                '854x480',
-            );
-            logger.log({
-                level: 'info',
-                label: 'handler:media',
-                message: `Video ${mediaId} converted to 480p`,
-            });
-        } catch (error) {
-            logger.log({
-                level: 'error',
-                label: 'handler:media',
-                message: `Failed to convert 480p video error="${error.message}"`,
-            });
-        }
-    }
-
-    await rename(originalVideoFilePath, join(rootDir, mediaId.toString(), `${videoHeight}p.mp4`));
-    logger.log({
-        level: 'info',
-        label: 'handler:media',
-        message: `Original video ${mediaId} renamed to ${videoHeight}p.mp4`,
+    proc.on('spawn', function () {
+        proc.send('preset');
     });
 
-    await updateMediaStatus(mediaId, 'done');
+    proc.on('error', function (error) {
+        logger.log({
+            level: 'error',
+            label: 'worker:media-parser',
+            message: error.message,
+        });
+    });
+
+    proc.on('message', async (message) => {
+        logger.log({
+            level: 'info',
+            label: 'worker:media-parser',
+            message: `Message received ${message}`,
+        });
+
+        if (message === 'preset:done') {
+            proc.send('parse');
+            await updateMediaStatus(mediaId, 'parsing');
+        } else if (message === 'parse:done') {
+            proc.send('done');
+            await updateMediaStatus(mediaId, 'done');
+        }
+    });
 
     reply.status(200).send({
         message: 'File saved',
+        result: {
+            id: mediaId,
+        },
     });
 }
