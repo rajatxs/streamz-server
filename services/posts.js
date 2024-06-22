b37848af8562e415057d78ec6ffd360afb07b1af
Rajat Sharma <rxx256@gmail.com>
2024-06-22 20:46:56 +0530

    replace media service with post service

---
+++ services/posts.js
@@ -0,0 +1,71 @@
+import { Post } from '../models/Post.js';
+import { getRow, getRows, insertRow, updateRow, deleteRow } from '../utils/sqlite.js';
+
+/**
+ * @param {number} id
+ * @returns {Promise<Post>}
+ */
+export async function getPost(id) {
+    const row = await getRow('SELECT * FROM posts_public_view WHERE id=?;', [id]);
+
+    if (!row) {
+        return null;
+    }
+    return Post.fromRow(row);
+}
+
+/**
+ * @param {Record<string, string|number|boolean>} [options]
+ * @returns {Promise<Post[]>}
+ */
+export async function getPostCollection(options = {}) {
+    const rows = await getRows('SELECT * FROM posts_public_view WHERE state=? LIMIT ?', [
+        options.state,
+        options.limit,
+    ]);
+
+    if (Array.isArray(rows)) {
+        return rows.map((row) => Post.fromRow(row));
+    } else {
+        return [];
+    }
+}
+
+/**
+ * @param {Pick<Post, 'title'|'description'|'public'|'userId'} data
+ * @returns {Promise<number>}
+ */
+export function createPost(data) {
+    return insertRow('INSERT INTO posts(title, desc, public, user_id) VALUES (?, ?, ?, ?);', [
+        data.title,
+        data.description,
+        data.public,
+        data.userId,
+    ]);
+}
+
+/**
+ * @param {number} id
+ * @returns {Promise<boolean>}
+ */
+export async function isPostExists(id) {
+    const row = await getRow('SELECT COUNT(id) as count FROM posts WHERE id=?;', [id]);
+    return row.count > 0;
+}
+
+/**
+ * @param {number} id
+ * @param {string} status
+ * @returns {Promise<number>}
+ */
+export async function updatePostState(id, status) {
+    return updateRow('UPDATE posts SET state=? WHERE id=?;', [status, id]);
+}
+
+/**
+ * @param {number} id
+ * @returns {Promise<number>}
+ */
+export function deletePost(id) {
+    return deleteRow('DELETE FROM posts WHERE id=?;', [id]);
+}
